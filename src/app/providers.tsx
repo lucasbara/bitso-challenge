9e01b8664c1d80edb5b7d671b9de4ca167729671
“Lucas <lucasbarallobre@gmail.com>
2024-09-18 20:25:09 +0200

    refactor: organize folders

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,7 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import config from './lib/wagmi';
+import { config } from '@/lib/wagmi';
 
 const queryClient = new QueryClient();
 
