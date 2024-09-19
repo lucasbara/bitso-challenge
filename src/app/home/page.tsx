5908e0fccd58f3fb4ada6cce867e2d8a70517976
“Lucas <lucasbarallobre@gmail.com>
2024-09-19 17:25:03 +0200

    feat: push to home if wallet disconnects

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import {
   useAccount,
@@ -15,20 +16,25 @@ import { toast } from 'react-toastify';
 
 import { contractConfig } from '@/lib/wagmi';
 import Logo from '@/svgs/logo.svg';
+import Spinner from '@/components/spinner';
 
 export default function Home() {
   const [recipientAddress, setRecipientAddress] = useState('');
   const [amount, setAmount] = useState<'' | number>('');
 
-  const { address } = useAccount();
+  const router = useRouter();
+  const { address, isConnected: isWalletConnected } = useAccount();
   const { refetch } = useBalance();
   const { data: balance } = useReadContract({
-    abi: contractConfig.abi,
-    address: contractConfig.address,
+    ...contractConfig,
     functionName: 'balanceOf',
     args: [address],
   });
   const { writeContractAsync } = useWriteContract();
+
+  useEffect(() => {
+    if (!isWalletConnected) router.push('/');
+  }, [isWalletConnected, router]);
 
   const handleTransfer = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -49,8 +55,7 @@ export default function Home() {
         toastId: 'transferPending',
       });
       const result = await writeContractAsync({
-        abi: contractConfig.abi,
-        address: contractConfig.address,
+        ...contractConfig,
         functionName: 'transfer',
         args: [recipientAddress, BigInt(amount * 1e18)], // Convert to wei (18 decimals)
       });
@@ -80,8 +85,7 @@ export default function Home() {
         toastId: 'claimPending',
       });
       const result = await writeContractAsync({
-        abi: contractConfig.abi,
-        address: contractConfig.address,
+        ...contractConfig,
         functionName: 'claimTokens',
       });
 
@@ -104,57 +108,65 @@ export default function Home() {
 
   return (
     <div className="bg-gray-900 min-h-screen text-white fonts-inter">
-      <header className="py-4 px-6 flex justify-between items-center border-b border-gray-700">
-        <Link href="/">
-          <Image width={125} height={125} alt="Bitso Logo" src={Logo} />
-        </Link>
-        <ConnectButton />
-      </header>
-      <main className="flex flex-col items-center justify-center min-h-[calc(100vh-150px)] px-4">
-        <div className="inline-block bg-gray-800 text-xs px-3 py-1 rounded-full mt-3 mb-6">
-          dApp Challenge
+      {!isWalletConnected ? (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Spinner />
         </div>
-        <h1 className="text-4xl font-bold mb-4 text-white">
-          Token Transfer
-          <br />
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-orange-500">
-            Send your crypto
-          </span>
-        </h1>
-        <p className="text-gray-400 mb-8">
-          {balance
-            ? `You have ${Number(balance) / 1e18} BTS`
-            : 'You have no tokens'}
-        </p>
-        <form onSubmit={handleTransfer} className="max-w-md w-full mb-8">
-          <input
-            type="text"
-            value={recipientAddress}
-            onChange={(e) => setRecipientAddress(e.target.value)}
-            placeholder="Recipient Address"
-            className="w-full mb-4 p-2 rounded bg-gray-800 text-white"
-          />
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(Number(e.target.value))}
-            placeholder="Amount"
-            className="w-full mb-4 p-2 rounded bg-gray-800 text-white"
-          />
-          <button
-            type="submit"
-            className="bg-lime-600 px-6 py-3 rounded-md hover:bg-lime-800 text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 w-full mb-4"
-          >
-            Send Tokens
-          </button>
-        </form>
-        <button
-          onClick={handleClaim}
-          className="bg-blue-600 px-6 py-3 rounded-md hover:bg-blue-800 text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 w-full max-w-md mb-8"
-        >
-          Claim 100 BTS Tokens
-        </button>
-      </main>
+      ) : (
+        <>
+          <header className="py-4 px-6 flex justify-between items-center border-b border-gray-700">
+            <Link href="/">
+              <Image width={125} height={125} alt="Bitso Logo" src={Logo} />
+            </Link>
+            <ConnectButton />
+          </header>
+          <main className="flex flex-col items-center justify-center min-h-[calc(100vh-150px)] px-4">
+            <div className="inline-block bg-gray-800 text-xs px-3 py-1 rounded-full mt-3 mb-6">
+              dApp Challenge
+            </div>
+            <h1 className="text-4xl font-bold mb-4 text-white">
+              Token Transfer
+              <br />
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-orange-500">
+                Send your crypto
+              </span>
+            </h1>
+            <p className="text-gray-400 mb-8">
+              {balance
+                ? `You have ${Number(balance) / 1e18} BTS`
+                : 'You have no tokens'}
+            </p>
+            <form onSubmit={handleTransfer} className="max-w-md w-full mb-8">
+              <input
+                type="text"
+                value={recipientAddress}
+                onChange={(e) => setRecipientAddress(e.target.value)}
+                placeholder="Recipient Address"
+                className="w-full mb-4 p-2 rounded bg-gray-800 text-white"
+              />
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(Number(e.target.value))}
+                placeholder="Amount"
+                className="w-full mb-4 p-2 rounded bg-gray-800 text-white"
+              />
+              <button
+                type="submit"
+                className="bg-lime-600 px-6 py-3 rounded-md hover:bg-lime-800 text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 w-full mb-4"
+              >
+                Send Tokens
+              </button>
+            </form>
+            <button
+              onClick={handleClaim}
+              className="bg-blue-600 px-6 py-3 rounded-md hover:bg-blue-800 text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 w-full max-w-md mb-8"
+            >
+              Claim 100 BTS Tokens
+            </button>
+          </main>
+        </>
+      )}
     </div>
   );
 }
