f89d6a220e65cd8aab4a00245ebf670fe5868a70
“Lucas <lucasbarallobre@gmail.com>
2024-09-19 16:53:00 +0200

    feat: handle transfer and claim tokens

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,26 +1,106 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
+
 import Image from 'next/image';
-import Logo from '@/svgs/logo.svg';
 import Link from 'next/link';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { useAccount, useWriteContract, useReadContract } from 'wagmi';
-import { contractConfig } from '@/lib/wagmi';
+import {
+  useAccount,
+  useWriteContract,
+  useReadContract,
+  useBalance,
+} from 'wagmi';
 import { toast } from 'react-toastify';
 
+import { contractConfig } from '@/lib/wagmi';
+import Logo from '@/svgs/logo.svg';
+
 export default function Home() {
+  const [recipientAddress, setRecipientAddress] = useState('');
+  const [amount, setAmount] = useState<'' | number>('');
+
   const { address } = useAccount();
+  const { refetch } = useBalance();
   const { data: balance } = useReadContract({
     abi: contractConfig.abi,
     address: contractConfig.address,
     functionName: 'balanceOf',
     args: [address],
   });
-  const { writeContract, writeContractAsync } = useWriteContract();
+  const { writeContractAsync } = useWriteContract();
 
-  const [recipientAddress, setRecipientAddress] = useState('');
-  const [amount, setAmount] = useState('');
+  const handleTransfer = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!recipientAddress) {
+      toast.error('Please enter a valid recipient address.');
+      return;
+    }
+
+    if (!amount || isNaN(amount) || amount <= 0) {
+      toast.error('Please enter a valid amount greater than zero.');
+      return;
+    }
+
+    try {
+      toast.info('Initiating transfer...', {
+        autoClose: false,
+        toastId: 'transferPending',
+      });
+      const result = await writeContractAsync({
+        abi: contractConfig.abi,
+        address: contractConfig.address,
+        functionName: 'transfer',
+        args: [recipientAddress, BigInt(amount * 1e18)], // Convert to wei (18 decimals)
+      });
+
+      if (!result) {
+        toast.error('Transaction failed. Check the console for details.');
+      } else {
+        toast.success(
+          'Transaction successful! You can view it on a block explorer.'
+        );
+        setRecipientAddress('');
+        setAmount('');
+      }
+    } catch (error) {
+      console.error('Unexpected error:', error);
+      toast.error('An unexpected error occurred. Please try again later.');
+    } finally {
+      toast.dismiss('transferPending');
+    }
+    refetch();
+  };
+
+  const handleClaim = async () => {
+    try {
+      toast.info('Claiming tokens...', {
+        autoClose: false,
+        toastId: 'claimPending',
+      });
+      const result = await writeContractAsync({
+        abi: contractConfig.abi,
+        address: contractConfig.address,
+        functionName: 'claimTokens',
+      });
+
+      if (!result) {
+        toast.error(
+          'Failed to claim tokens. Please check the console for details.'
+        );
+      } else {
+        toast.success('Tokens claimed successfully! 🦄');
+      }
+    } catch {
+      toast.error(
+        'An unexpected error occurred while claiming tokens. Please try again later.'
+      );
+    } finally {
+      toast.dismiss('claimPending');
+    }
+    refetch();
+  };
 
   return (
     <div className="bg-gray-900 min-h-screen text-white fonts-inter">
@@ -42,7 +122,9 @@ export default function Home() {
           </span>
         </h1>
         <p className="text-gray-400 mb-8">
-          {balance ? `You have ${balance} BTS` : 'You have no tokens'}
+          {balance
+            ? `You have ${Number(balance) / 1e18} BTS`
+            : 'You have no tokens'}
         </p>
         <form onSubmit={handleTransfer} className="max-w-md w-full mb-8">
           <input
@@ -55,7 +137,7 @@ export default function Home() {
           <input
             type="number"
             value={amount}
-            onChange={(e) => setAmount(e.target.value)}
+            onChange={(e) => setAmount(Number(e.target.value))}
             placeholder="Amount"
             className="w-full mb-4 p-2 rounded bg-gray-800 text-white"
           />
